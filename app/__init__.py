42374d229db7c9923a8d85e0ac468c9f6be0be68
frankiecancino <frankiecancino@gmail.com>
2018-11-29 21:08:27 -0600

    fixing bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__, static_url_path='')
 
 
 @app.route('/')
+@app.route('/index')
+@app.route('/main')
 def index():
     return render_template('main.html')
 
